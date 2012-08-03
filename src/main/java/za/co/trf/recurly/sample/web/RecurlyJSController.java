7859dc9448fc022f2717b349c7f7f68755aa1d27
Terence Foxcroft <terence.foxcroft@bsg.co.za>
2012-08-03 17:54:45 +0200

    Samples WIP

--- src/main/java/za/co/trf/recurly/sample/web/RecurlyJSController.java
+++ src/main/java/za/co/trf/recurly/sample/web/RecurlyJSController.java
@@ -1,12 +1,21 @@
 package za.co.trf.recurly.sample.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import za.co.trf.recurly.RecurlyConstants;
+import za.co.trf.recurly.js.RecurlyJS;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RequestMapping("/recurly/js/**")
 @Controller
 public class RecurlyJSController {
+
+    @Autowired
+    private RecurlyJS recurlyJS;
 
     @RequestMapping
     public String index() {
@@ -15,12 +24,27 @@ public class RecurlyJSController {
 
     @RequestMapping("subscribe-to-plan")
     public String subscribeToPlan(Model model) {
-        model.addAttribute("signature", "needs-server-support-code");
+        String signature = recurlyJS.sign(new HashMap<String, Object>() {{
+            put(RecurlyConstants.JS_PARAM_ACCOUNT, new HashMap<String, Object>() {
+                {
+                    put(RecurlyConstants.JS_PARAM_ACCOUNT_CODE, "123");
+                }
+            });
+        }});
+        model.addAttribute("signature", signature);
         return "recurly/js/subscribe-to-plan";
     }
 
     @RequestMapping("update-billing-info")
-    public String updateBillingInfo() {
+    public String updateBillingInfo(Model model) {
+        String signature = recurlyJS.sign(new HashMap<String, Object>() {{
+            put(RecurlyConstants.JS_PARAM_ACCOUNT, new HashMap<String, Object>() {
+                {
+                    put(RecurlyConstants.JS_PARAM_ACCOUNT_CODE, "123");
+                }
+            });
+        }});
+        model.addAttribute("signature", signature);
         return "recurly/js/update-billing-info";
     }
 
@@ -28,6 +52,10 @@ public class RecurlyJSController {
     public String oneTimeTransaction() {
         return "recurly/js/one-time-transaction";
     }
-    
+
+    @RequestMapping("terms-and-conditions")
+    public String termsAndConditions() {
+        return "recurly/js/terms-and-conditions";
+    }
 
 }
