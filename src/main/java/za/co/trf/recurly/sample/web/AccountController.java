5ddbbbd9049bfa8d6d586cbba68e59e6b84a31f7
Terence Foxcroft <terence.foxcroft@bsg.co.za>
2012-08-04 22:27:16 +0200

    Samples WIP

--- src/main/java/za/co/trf/recurly/sample/web/AccountController.java
+++ src/main/java/za/co/trf/recurly/sample/web/AccountController.java
@@ -1,23 +1,26 @@
 package za.co.trf.recurly.sample.web;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import za.co.trf.recurly.api.RecurlyAccountService;
+import za.co.trf.recurly.api.entity.Account;
+
+import java.util.List;
 
 @RequestMapping("/recurly/accounts/**")
 @Controller
 public class AccountController {
 
-    @RequestMapping(method = RequestMethod.POST, value = "{id}")
-    public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
-    }
+    @Autowired
+    private RecurlyAccountService recurlyAccountService;
 
     @RequestMapping
-    public String index() {
+    public String index(Model model) {
+        List<Account> accounts = recurlyAccountService.getAllAccounts();
+        model.addAttribute("accounts", accounts);
         return "recurly/accounts/index";
     }
+
 }
